57f1c36f712801ef98f2545942a5934bce9fa86d
hkohko <hekodev.py@gmail.com>
2024-03-12 12:01:31 +0700

    major refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 use clap::Parser;
-use crossbeam::deque::Worker;
+use crossbeam::deque::{Stealer, Worker};
 use std::env::current_dir;
 use std::io;
+use std::path::Path;
+use std::thread;
 use std::{fs::DirEntry, path::PathBuf};
+use turbojpeg::{compress_image, decompress_image, Subsamp::Sub2x2};
 #[derive(Parser, Debug)]
 pub struct TaskArgs {
     /// Quality in ratio to the original.
@@ -11,7 +14,8 @@ pub struct TaskArgs {
     /// The output directory of compressed images.
     #[arg(default_value_t = format!("compressed"))]
     output_dir: String,
-    #[arg(short, default_value_t = 2)]
+    /// The number of worker threads used.
+    #[arg(short, default_value_t = 4)]
     device: u8,
 }
 impl TaskArgs {
@@ -37,15 +41,29 @@ impl Tasks {
         self.queue
     }
     pub fn get_device(&self) -> i32 {
-        self.device_num.into()
+        let device = self.device_num - 1;
+        device.into()
     }
     pub fn get_output_dir(&self) -> PathBuf {
         self.output_dir.clone()
     }
+    pub fn get_task_amount(&self) -> usize {
+        let task_amount = {
+            if self.device_num > 1 {
+                let as_f64 =
+                    self.queue.len() as f64 / f64::try_from(self.device_num).unwrap().ceil() + 1.0;
+                as_f64 as usize
+            } else {
+                eprintln!("Minimum amount of device: 2");
+                std::process::exit(1)
+            }
+        };
+        task_amount
+    }
     fn get_tasks(cur_dir: &PathBuf) -> io::Result<Worker<Option<DirEntry>>> {
         let read_dir = std::fs::read_dir(cur_dir)?;
         let worker = Worker::new_fifo();
-        let tasks = read_dir
+        let _tasks = read_dir
             .map(|direntry| worker.push(direntry.ok()))
             .collect::<Vec<_>>();
         Ok(worker)
@@ -56,5 +74,82 @@ impl Tasks {
             std::fs::create_dir(output_dir).expect("Cannot create output dir {output_dir}\n");
         }
         output_path
+    }
+}
+pub struct TaskWorker<'a> {
+    device_num: i32,
+    quality: i32,
+    dir_name: PathBuf,
+    stealer: &'a Stealer<Option<DirEntry>>,
+    task_amount: usize,
+}
+impl<'a> TaskWorker<'a> {
+    pub fn new(
+        device_num: i32,
+        quality: i32,
+        dir_name: PathBuf,
+        stealer: &'a Stealer<Option<DirEntry>>,
+        task_amount: usize,
+    ) -> Self {
+        Self {
+            device_num,
+            quality,
+            dir_name,
+            stealer,
+            task_amount,
+        }
+    }
+    pub fn send_to_threads(self) -> Vec<thread::JoinHandle<()>> {
+        let mut handles = vec![];
+        for id in 0..self.device_num {
+            let thread_dir_name = self.dir_name.clone();
+            let thread_worker = Worker::new_fifo();
+            let _thread_stealer = self
+                .stealer
+                .steal_batch_with_limit(&thread_worker, self.task_amount);
+            let handle = thread::spawn(move || {
+                while let Some(direntry) = thread_worker.pop() {
+                    Compress::do_work(direntry, thread_dir_name.clone(), self.quality, id + 1);
+                }
+            });
+            handles.push(handle);
+        }
+        return handles;
+    }
+}
+pub struct Compress {}
+impl Compress {
+    pub fn do_work(direntry: Option<DirEntry>, dir_name: PathBuf, quality: i32, worker: i32) {
+        let Some(val_direntry) = direntry else {
+            return;
+        };
+        match Compress::compress(val_direntry.path(), dir_name, quality, worker) {
+            Err(e) => {
+                eprintln!("{e}");
+            }
+            Ok(msg) => {
+                println!("{msg}");
+            }
+        };
+    }
+    fn compress<T>(p: T, dir: PathBuf, q: i32, worker: i32) -> anyhow::Result<String>
+    where
+        T: AsRef<Path>,
+    {
+        let path_as_ref = p.as_ref();
+        let filename = path_as_ref.file_name().unwrap_or_default();
+        let read = std::fs::read(path_as_ref)?;
+        let image: image::RgbImage = decompress_image(&read)?;
+        let jpeg_data = compress_image(&image, q, Sub2x2)?;
+        std::fs::write(dir.join(filename), jpeg_data)?;
+        let success_msg = format!(
+            "done: {} (worker {})",
+            path_as_ref
+                .file_name()
+                .unwrap_or_default()
+                .to_string_lossy(),
+            worker
+        );
+        Ok(success_msg)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
-use anyhow;
 use clap::Parser;
-use crossbeam::deque::Worker;
-use smoljpg::{TaskArgs, Tasks};
-use std::fs::DirEntry;
+use crossbeam::deque::Steal;
+use smoljpg::{Compress, TaskArgs, TaskWorker, Tasks};
 use std::io;
-use std::path::{Path, PathBuf};
-use std::thread;
-use turbojpeg::{compress_image, decompress_image, Subsamp::Sub2x2};
 fn main() {
     let args = TaskArgs::parse();
     if let Err(e) = spawn_workers(args) {
@@ -17,69 +12,23 @@ fn spawn_workers(args: TaskArgs) -> io::Result<()> {
     let create_task = Tasks::create(&args)?;
     let device_num = create_task.get_device();
     let dir_name = create_task.get_output_dir();
+    let task_amount = create_task.get_task_amount();
+    let quality = args.get_quality();
     let main_worker = create_task.get_main_worker();
-    let task_amount = {
-        let as_f64 = main_worker.len() as f64 / f64::try_from(device_num).unwrap().ceil();
-        as_f64 as usize
-    };
     let main_stealer = main_worker.stealer();
-    let clone_dir_name = dir_name.clone();
-    let mut handles = vec![];
-    for id in 0..device_num {
-        let thread_dir_name = clone_dir_name.clone();
-        let thread_worker = Worker::new_fifo();
-        let _thread_stealer = main_stealer.steal_batch_with_limit(&thread_worker, task_amount);
-        let quality = args.get_quality();
-        let handle = thread::spawn(move || {
-            while let Some(direntry) = thread_worker.pop() {
-                do_work(direntry, thread_dir_name.clone(), quality, id);
-            }
-        });
-        handles.push(handle);
+    let handles = TaskWorker::new(
+        device_num,
+        quality,
+        dir_name.clone(),
+        &main_stealer,
+        task_amount,
+    )
+    .send_to_threads();
+    while let Steal::Success(direntry) = main_stealer.steal() {
+        Compress::do_work(direntry, dir_name.clone(), quality, 0);
     }
     for h in handles.into_iter() {
         h.join().unwrap();
     }
     Ok(())
-}
-fn do_work(direntry: Option<DirEntry>, dir_name: PathBuf, quality: i32, worker: i32) {
-    let Some(val_direntry) = direntry else {
-        return;
-    };
-    if val_direntry
-        .path()
-        .extension()
-        .unwrap_or_default()
-        .to_ascii_lowercase()
-        == "jpg"
-    {
-        match compress(val_direntry.path(), dir_name, quality, worker) {
-            Err(e) => {
-                eprintln!("{e}");
-            }
-            Ok(msg) => {
-                println!("{msg}");
-            }
-        };
-    }
-}
-fn compress<T>(p: T, dir: PathBuf, q: i32, worker: i32) -> anyhow::Result<String>
-where
-    T: AsRef<Path>,
-{
-    let path_as_ref = p.as_ref();
-    let filename = path_as_ref.file_name().unwrap_or_default();
-    let read = std::fs::read(path_as_ref)?;
-    let image: image::RgbImage = decompress_image(&read)?;
-    let jpeg_data = compress_image(&image, q, Sub2x2)?;
-    std::fs::write(dir.join(filename), jpeg_data)?;
-    let success_msg = format!(
-        "done: {} (worker {})",
-        path_as_ref
-            .file_name()
-            .unwrap_or_default()
-            .to_string_lossy(),
-        worker
-    );
-    Ok(success_msg)
 }
