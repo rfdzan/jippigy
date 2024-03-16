ff4fd88d45d60f0c0bcd0887f302e33cdf56596e
hkohko <hekodev.py@gmail.com>
2024-03-16 09:51:01 +0700

    scopes the lock instead of a manual drop

--- src/threads.rs
+++ src/threads.rs
@@ -35,44 +35,43 @@ impl TaskWorker {
             let local_stealer = Arc::clone(&to_steal_from);
             let thread_dir_name = self.dir_name.clone();
             let handle = thread::spawn(move || {
+                let mut are_queues_empty = Vec::with_capacity(usize::from(self.device_num));
+                let mut payload = Vec::with_capacity(1);
                 // wait a bit for the main worker to have something in it.
                 thread::sleep(time::Duration::from_millis(1));
-                let mut queues_empty = Vec::with_capacity(usize::from(self.device_num));
                 loop {
-                    let gain_lock = local_stealer.try_lock().ok();
-                    let Some(lock_list_of_stealers) = gain_lock else {
-                        continue;
-                    };
-                    let mut temp_stealer = Vec::with_capacity(1);
-                    for stealer in lock_list_of_stealers.iter() {
-                        let Steal::Success(direntry) = stealer.steal() else {
+                    {
+                        let Some(stealer_guard) = local_stealer.try_lock().ok() else {
                             continue;
                         };
-                        temp_stealer.push(direntry);
-                        break;
+                        for stealer in stealer_guard.iter() {
+                            let Steal::Success(direntry) = stealer.steal() else {
+                                continue;
+                            };
+                            payload.push(direntry);
+                            break;
+                        }
+                        let _checks = stealer_guard
+                            .iter()
+                            .map(|stealer| {
+                                if stealer.is_empty() {
+                                    are_queues_empty.push(true);
+                                } else {
+                                    are_queues_empty.push(false);
+                                }
+                            })
+                            .collect::<Vec<_>>();
+                        // lock is no longer needed past this point
                     }
-                    let _checks = lock_list_of_stealers
-                        .iter()
-                        .map(|stealer| {
-                            if stealer.is_empty() {
-                                queues_empty.push(true);
-                            } else {
-                                queues_empty.push(false);
-                            }
-                        })
-                        .collect::<Vec<_>>();
-                    // Release lock before doing compression.
-                    drop(lock_list_of_stealers);
-                    if let Some(direntry) = temp_stealer.pop() {
+                    if let Some(direntry) = payload.pop() {
                         Compress::new(direntry, thread_dir_name.clone(), self.quality).do_work();
                     }
-                    // If all worker threads have exhausted their queue,
-                    // exit this loop
-                    if queues_empty.iter().all(|val| val == &true) {
-                        println!("a thread just exited");
+                    // if all stealers are empty, exit the loop.
+                    if are_queues_empty.iter().all(|val| val == &true) {
                         break;
                     }
-                    queues_empty.clear();
+                    are_queues_empty.clear();
+                    payload.clear();
                 }
             });
             handles.push(handle);
